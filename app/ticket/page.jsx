a102e26a9b71c876c609b157b77626ff34a26168
Gopika011 <gopikaa011@gmail.com>
2024-12-15 22:02:44 +0530

    ticket design fix

--- app/ticket/page.jsx
+++ app/ticket/page.jsx
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot,faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import peg from '@/app/assets/peg.png'
 import Image from "next/image";
+import PunchCircles from "./component/PunchCircles";
 
 
 const Ticket = () => {
@@ -58,89 +59,80 @@ const Ticket = () => {
 
   return (
     <div className="min-h-screen w-full bg-[#070707] font-pressstart flex justify-center items-center">
-    {/* <div className="bg-primary/5 w-full h-10 top-0 left-0 p-10 flex justify-between bg-black"></div> */}
 
     <div className="md:h-[88%] overflow-auto w-full flex flex-grow flex-col justify-center items-center pb-20 md:pb-0 ">
-      <div className="shadow-lg md:h-[50%] mt-10 md:mt-0 w-4/5 md:w-3/5  flex-col md:flex-row flex justify-center items-center bg-white">
+      <div className="shadow-lg md:h-[50%] mt-10 md:mt-0 w-4/5 md:w-3/5 flex-col lg:flex-row flex justify-center items-center bg-white relative">
         
-        <div className="md:w-1/2 md:border-r-2 border-b-2 md:border-b-0 p-6 border-dashed border-black flex-col h-full flex justify-center items-center relative">
+        {/* <div className="md:w-1/2 p-6 flex-col h-full flex justify-center items-center relative">      
           <div className="bg-black rounded-xl w-full h-full relative">
                 <div className="m-6"><Image src={peg} alt='pegasus' className="object-contain"/></div>
                 <h2 className="absolute right-3 bottom-3 text-white text-md">JAN 10</h2>
           </div>
-          <div className=" py-2 -left-5  top-0 absolute hidden h-full w-8 md:flex md:flex-col gap-5">
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
+        </div> */}
+        
+        <div className="lg:w-1/2 p-6 flex-col h-full flex justify-center items-center relative">      
+          <div className="bg-black rounded-xl w-full h-full relative">
+                <div className="m-6"><Image src={peg} alt='pegasus' className="object-contain"/></div>
+                <h2 className="absolute right-3 bottom-3 text-white text-md">JAN 10</h2>
           </div>
-
-          
         </div>
 
-
-        <div className="w-full h-full relative text-center">
-          <div className="w-full h-1/5 p-10 pb-0">
-            <p className="text-xl py-2 md:py-0 font-bold text-black outline-text-red">
+        <div className="p-2 md:px-10 w-full h-full relative text-center lg:text-start lg:border-l-2 border-t-2 lg:border-t-0 border-black border-dashed">
+          <div className="w-full h-1/5  pt-5 pb-0">
+            <p className="text-lg md:text-xl py-2 md:py-0 font-bold text-black outline-text-red" style={{WebkitTextStroke: '0.1px #D71015'}}>
               TICKET DETAILS
             </p>
           </div>
 
-          <div className="flex flex-col gap-4 md:gap-2 md:p-10 py-10 justify-center h-4/5">
+          <div className="flex flex-col gap-4 md:gap-2 md:pt-4 py-10 justify-center h-4/5">
               <div className="flex md:flex-row flex-col text-center justify-between md:items-center">
-               <p className="text-sm md:text-md ">NAME</p>
-               <p className="text-sm md:text-md">
+               <p className="ticket-text ">NAME</p>
+               <p className="ticket-text" >
                  {userDetails.fname} {userDetails.lname}
                </p>
               </div>
 
               <div className="flex md:flex-row flex-col text-center justify-between items-center">
-                <p className="text-sm md:text-md">BRANCH & SEM</p>
-                <p className="text-sm md:text-md">{userDetails.branch}</p>
+                <p className="ticket-text">BRANCH & SEM</p>
+                <p className="ticket-text">{userDetails.branch}</p>
               </div>
 
               <div className="flex md:flex-row flex-col text-center justify-between items-center">
-                <p className="text-sm md:text-md">PHONE NO</p>
-                <p className="text-sm md:text-md">+91 {userDetails.phone}</p>
+                <p className="ticket-text">PHONE NO</p>
+                <p className="ticket-text">+91 {userDetails.phone}</p>
               </div>
 
               <div className="flex md:flex-row flex-col text-center justify-between items-center">
-                <p className="text-sm md:text-md">VENUE</p>
-                <p className="text-sm md:text-md">MITS KOCHI</p>
+                <p className="ticket-text">VENUE</p>
+                <p className="ticket-text">MITS KOCHI</p>
               </div>
 
               <div className="flex md:flex-row flex-col text-center justify-between items-center">
-                <p className="text-sm md:text-md">DATES</p>
-                <p className="text-sm md:text-md">
+                <p className="ticket-text">DATES</p>
+                <p className="ticket-text">
                   JANUARY 10,11
                 </p>
               </div>
               <div className="flex md:flex-row flex-col text-center justify-between items-center">
-                <p className="text-sm md:text-md">TIME</p>
-                <p className="text-sm md:text-md">9AM-5PM</p>
+                <p className="ticket-text">TIME</p>
+                <p className="ticket-text">9AM-5PM</p>
               </div>
 
-              <div className="flex md:flex-row flex-col text-center justify-between items-center">
-                <p className="text-sm md:text-md">FOOD PREFERENCE</p>
-                <p className="text-sm md:text-md">{userDetails.foodPref}</p>
+              <div className="flex md:flex-row flex-col text-center justify-between items-center pb-14 md:pb-2">
+                <p className="ticket-text">FOOD PREFERENCE</p>
+                <p className="ticket-text">{userDetails.foodPref}</p>
               </div>
 
-              <div className="flex justify-end absolute bottom-1 right-6">
-              <h2 className="text-sm text-[#D71015]">No.{userDetails.ticketNumber}</h2>
-              </div>
-          </div>
 
-          <div className=" py-2 -right-5 top-0 absolute hidden h-full w-8 md:flex md:flex-col gap-5">
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
-          <div className="rounded-full h-8 w-8 bg-[#070707]"></div>
+              <div className="w-full absolute bottom-1 md:pr-16 pr-5 flex flex-col md:flex-row md:justify-between">
+              <h2 className="text-lg font-extrabold text-[#D71015] font-mono">No.{userDetails.ticketNumber}</h2>
+              <h2 className="text-lg font-extrabold text-[#D71015] font-mono">Status: Confirmed</h2>
+              </div>
           </div>
         </div>
+
+
+        <PunchCircles/>
       </div>
     </div>
   </div>
